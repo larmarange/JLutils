12ed1a6904b259051b0a0bd8fc716935bf2e270a
Joseph <joseph@larmarange.net>
2013-12-14 01:09:36 +0200

    documenting s.freq

--- R/factor_analysis.R
+++ R/factor_analysis.R
@@ -1,6 +1,21 @@
-#' Display frequencies on the factorial plan
+#' Plot of factorial maps with frequency of each point
+#' 
+#' For each point on the factorial plan, display the number of identical 
+#' observations (i.e. with the same coordinates).
+#' 
+#' @param dfxy a data frame with two coordinates
+#' @param xax	column for the x axis
+#' @param yax	column for the y axis
+#' @param ... additional parameters sent to \code{\link{s.value}}
 #' @source \url{http://joseph.larmarange.net/?Representer-des-effectifs-dans-le}
 #' @export s.freq
+#' @examples
+#' if (require(ade4) & require(questionr)) {
+#'   data(hdv2003)
+#'   acm <- dudi.acm(hdv2003[,c("sexe","sport","bricol","cuisine","cinema")], scannf = FALSE)
+#'   s.label(acm$li, clabel = 0, cpoint = 2)
+#'   s.freq(acm$li, csize = 0.75)
+#' }
 
 s.freq <-
 	function(dfxy, xax=1, yax=2, ...)
@@ -10,5 +25,5 @@ s.freq <-
 		d <- d[d$Freq>0,]
 		d[1] <- as.numeric(as.character(d[[1]]))
 		d[2] <- as.numeric(as.character(d[[2]]))
-		s.value(d,d$Freq, ...)
+		s.value(d, d$Freq, ...)
 	}
