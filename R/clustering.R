a4fc23691b05b2732ddf17a5071cde2b8204864e
Joseph <joseph@larmarange.net>
2013-12-13 18:36:27 +0200

    documenting best.cutree

--- R/clustering.R
+++ R/clustering.R
@@ -1,5 +1,23 @@
 #' Optimal partition based on the higher relative loss of inertia
-#' @source \url{http://joseph.larmarange.net/?Ou-couper-un-dendrogramme}
+#' 
+#' This function calculates the best partition to cut a dendrogram based on
+#' the higher relative loss of inertia criteria. This criteria was originaly
+#' proposed by the \code{\link[FactoMineR]{HCPC}} function of the package
+#' \code{FactoMineR}.
+#' 
+#' @param hc a clustering tree (an object of class \code{"hclust"}, \code{"dendrogram"} or \code{"agnes"})
+#' @param min the minimum number of classes
+#' @param max the maximum number of classes
+#' @param loss if \code{TRUE}, will return the relative loss of inertia of each partition instead of the best partition
+#' @param graph if \code{TRUE}, will plot the relative loss of inertia of each partition, the best partition being indicated in black and the second best in grey
+#' @param ... additional arguments sent to \code{plot} (if \code{graph = TRUE})
+#' @seealso \code{\link[FactoMineR]{HCPC}}
+#' @examples
+#' hc <- hclust(dist(USArrests), "ave")
+#' best.cutree(hc)
+#' best.cutree(hc, loss = TRUE)
+#' best.cutree(hc, graph = TRUE)
+#' best.cutree(hc, graph = TRUE, min = 6, max = 15)
 #' @export best.cutree
 
 best.cutree <- 
@@ -19,10 +37,9 @@ function(hc, min=3, max=20, loss=FALSE, graph=FALSE, ...){
 		pch[best] <- 16
 		pch[best2] <- 21
 		plot(min:max, relative.loss, pch=pch, bg="grey75", ...)
-	} else {
-		if (loss)
-			relative.loss
-		else
-			best + min - 1
 	}
+	if (loss)
+		relative.loss
+	else
+		best + min - 1
 }
