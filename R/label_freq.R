b5e425de14e8045f7ec1f20f8148413b507001f5
Joseph <joseph@larmarange.net>
2015-05-24 11:55:41 +0200

    préférons labelled avec deux l

--- R/label_freq.R
+++ R/label_freq.R
@@ -4,13 +4,13 @@
 #' labels if they are available (typically if the data frame has been imported with
 #' \code{\link[foreign]{read.spss}} or \code{\link[foreign]{read.dta}})
 #' 
-#' @param labeled_x a formula (see \code{\link[stats]{xtabs}}) or a vector
-#' @param labeled_d a data.frame (if \code{labeled_x} is a formula) or an optional second vector
+#' @param labelled_x a formula (see \code{\link[stats]{xtabs}}) or a vector
+#' @param labelled_d a data.frame (if \code{labelled_x} is a formula) or an optional second vector
 #' @param add.variable.labels add variable labels to dimension names?
 #' @param ... additional parameters sent to \code{\link[stats]{xtabs}}
 #' 
 #' @details This function is particulary useful when the data has been imported
-#' from SPSS or Stata but the labeled variables not converted to factors.
+#' from SPSS or Stata but the labelled variables not converted to factors.
 #' 
 #' It's possible to enter \code{ltabs(df$Sex)} for convenient purpose. In this
 #' situation, the function will try to guess what is the corresponding data frame
@@ -30,21 +30,21 @@
 #' 
 #' @export
 
-ltabs <- function(labeled_x, labeled_df = NULL, add.variable.labels = FALSE, ...) {
-  if (inherits(labeled_x, "formula")) {
-    if(!is.data.frame(labeled_df)) stop("When labeled_x is a formula, labeled_df should be a data.frame.")
-    vars <- attr(terms(labeled_x), "term.labels")
-    df <- labeled_df
-    f <- labeled_x
+ltabs <- function(labelled_x, labelled_df = NULL, add.variable.labels = FALSE, ...) {
+  if (inherits(labelled_x, "formula")) {
+    if(!is.data.frame(labelled_df)) stop("When labelled_x is a formula, labelled_df should be a data.frame.")
+    vars <- attr(terms(labelled_x), "term.labels")
+    df <- labelled_df
+    f <- labelled_x
     for (var in vars)
       if (!var %in% names(df))
-        stop(paste(var, "not found in labeled_df"))
+        stop(paste(var, "not found in labelled_df"))
   } else {
     vars <- NULL
-    dx <- deparse(substitute(labeled_x))
+    dx <- deparse(substitute(labelled_x))
     posx <- regexpr("[$]", dx)[1]
     
-    if (is.null(labeled_df)) {
+    if (is.null(labelled_df)) {
       if(posx > 1) {
         df <- get(substr(dx, 1, posx-1))
         vars <- substr(dx, posx+1, nchar(dx))
@@ -54,7 +54,7 @@ ltabs <- function(labeled_x, labeled_df = NULL, add.variable.labels = FALSE, ...
         f <- paste("~", dx)
       }
     } else {
-      dd <- deparse(substitute(labeled_df))
+      dd <- deparse(substitute(labelled_df))
       posd <- regexpr("[$]", dd)[1]
       
       if (posx > 1 & posd > 1 & substr(dx, 1, posx-1) == substr(dd, 1, posd-1)) { ## If same data.frame
@@ -87,11 +87,11 @@ for (var in vars) df[[var]] <- lfactor(var, df)
 #' 
 #' This function display a frequency table with value labels if they are available
 #' 
-#' @param labeled_x a vector
+#' @param labelled_x a vector
 #' @param ... additional arguments sent to \code{\link[questionr]{freq}}
 #' 
 #' @details This function is particulary useful when the data has been imported
-#' from SPSS or Stata but the labeled variables not converted to factors.
+#' from SPSS or Stata but the labelled variables not converted to factors.
 #' 
 #' \code{lfreq(df$Sex)} has been designed for convenient purpose. In this
 #' situation, the function will try to guess what is the corresponding data frame
@@ -110,9 +110,9 @@ for (var in vars) df[[var]] <- lfactor(var, df)
 #' 
 #' @export
 
-lfreq <- function(labeled_x, ...) {
+lfreq <- function(labelled_x, ...) {
   if (!require(questionr)) stop("questionr package is required")
-  dx <- deparse(substitute(labeled_x))
+  dx <- deparse(substitute(labelled_x))
   posx <- regexpr("[$]", dx)[1]
   if (posx > 1) {
     df <- get(substr(dx, 1, posx-1))
@@ -120,28 +120,28 @@ lfreq <- function(labeled_x, ...) {
     f <- as.formula(paste("~", var))
     return(freq(ltabs(f, df), ...))
   } else {
-    return(freq(labeled_x, ...))
+    return(freq(labelled_x, ...))
   }
 }
 
-#' Labeled factor
+#' labelled factor
 #' 
 #' Convert to a factor using SPSS/Stata value labels if they are available
 #' in the data frame
 #' 
-#' @param labeled_x the name of a column or a vector
-#' @param labeled_df a data frame (if \code{labeled_x} is the column name)
+#' @param labelled_x the name of a column or a vector
+#' @param labelled_df a data frame (if \code{labelled_x} is the column name)
 #' @param add.codes add the numerical code to the labels?
 #' 
 #' @details This function is particulary useful when the data has been imported
-#' from SPSS or Stata but the labeled variables not converted to factors.
+#' from SPSS or Stata but the labelled variables not converted to factors.
 #' 
 #' It's possible to enter \code{lfactor(df$Sex)} for convenient purpose. In this
 #' situation, the function will try to guess what is the corresponding data frame
 #' (i.e. \code{df}) by analysing the function call. To avoid any issue when 
 #' programming, you should prefer \code{lfactor("Sex", df)}.
 #' 
-#' @return If \code{labeled_x} is already a factor, \code{labeled_x} is
+#' @return If \code{labelled_x} is already a factor, \code{labelled_x} is
 #' returned with no change.
 #' 
 #' @seealso \code{\link{ltabs}}, \code{\link{lfreq}}
@@ -154,30 +154,30 @@ lfreq <- function(labeled_x, ...) {
 #' 
 #' @export
 
-lfactor <- function(labeled_x, labeled_df = NULL, add.codes = TRUE) {
-  if (length(labeled_x)>1) {
-    dx <- deparse(substitute(labeled_x))
+lfactor <- function(labelled_x, labelled_df = NULL, add.codes = TRUE) {
+  if (length(labelled_x)>1) {
+    dx <- deparse(substitute(labelled_x))
     posx <- regexpr("[$]", dx)[1]
     if (posx > 1) {
-      labeled_df <- get(substr(dx, 1, posx-1))
-      labeled_x <- substr(dx, posx+1, nchar(dx))
+      labelled_df <- get(substr(dx, 1, posx-1))
+      labelled_x <- substr(dx, posx+1, nchar(dx))
     } else {
-      return(labeled_x)
+      return(labelled_x)
     }
   }
   
-  if (!is.data.frame(labeled_df)) stop("labeled_df should be a data frame.")
+  if (!is.data.frame(labelled_df)) stop("labelled_df should be a data frame.")
   
-  if (is.factor(labeled_df[[labeled_x]])) return (labeled_df[[labeled_x]])
+  if (is.factor(labelled_df[[labelled_x]])) return (labelled_df[[labelled_x]])
   
   lab <- NULL
-  if (!is.null(attr(labeled_df[[labeled_x]], "value.labels")))
-    lab <- attr(labeled_df[[labeled_x]], "value.labels")
-  if (!is.null(attr(labeled_df, "label.table")[[labeled_x]]))
-    lab <- attr(labeled_df, "label.table")[[labeled_x]]
+  if (!is.null(attr(labelled_df[[labelled_x]], "value.labels")))
+    lab <- attr(labelled_df[[labelled_x]], "value.labels")
+  if (!is.null(attr(labelled_df, "label.table")[[labelled_x]]))
+    lab <- attr(labelled_df, "label.table")[[labelled_x]]
   
   if (!is.null(lab)) {
-    tmp <- data.frame(values = na.rm(unique(labeled_df[[labeled_x]])))
+    tmp <- data.frame(values = na.rm(unique(labelled_df[[labelled_x]])))
     tmp <- merge(tmp, data.frame(values = lab, vlab = names(lab)), all.x = TRUE)
     if (add.codes)
       tmp$flab <- paste0("[", tmp$values, "] ", tmp$vlab)
@@ -185,10 +185,10 @@ lfactor <- function(labeled_x, labeled_df = NULL, add.codes = TRUE) {
       tmp$flab <- tmp$vlab
     tmp[is.na(tmp$vlab),"flab"] <- as.character(tmp[is.na(tmp$vlab),"values"])
     
-    res <- factor(labeled_df[[labeled_x]], levels = tmp$values, labels = tmp$flab)
+    res <- factor(labelled_df[[labelled_x]], levels = tmp$values, labels = tmp$flab)
     return (res)
   } else {
-    return(factor(labeled_df[[labeled_x]]))
+    return(factor(labelled_df[[labelled_x]]))
   } 
 }
 
