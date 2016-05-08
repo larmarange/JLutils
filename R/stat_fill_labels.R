b6c9d168463afd68bb7ea38683008a9fce9f0c3e
larmarange <joseph@larmarange.net>
2016-05-08 21:17:14 +0200

    few adjustments

--- R/stat_fill_labels.R
+++ R/stat_fill_labels.R
@@ -16,9 +16,9 @@ StatFillLabels <- ggproto("StatFillLabels", StatCount,
                          cbind(new, unique[rep(1, nrow(new)), missing, drop = FALSE])
                        }, stats, groups, SIMPLIFY = FALSE)
                        res <- do.call(plyr::rbind.fill, stats)
-                       plyr::ddply(res, "x", plyr::mutate, prop = count/sum(count), y = (cumsum(count) - count / 2)/sum(count), na.rm = TRUE)
+                       plyr::ddply(res, "x", plyr::mutate, prop = count/sum(count), ylabel = (cumsum(count) - count / 2)/sum(count), na.rm = TRUE)
                      },
-                     default_aes = aes(y = ..y.., label = paste0(round(100 * ..prop.., digits =1), "%")) 
+                     default_aes = aes(y = ..ylabel.., label = paste0(round(100 * ..prop.., digits =1), "%")) 
 )
 
 #' Computes labels for position = "fill"
@@ -26,15 +26,16 @@ StatFillLabels <- ggproto("StatFillLabels", StatCount,
 #' This stat makes it easier to display labels with \code{position_fill}.
 #' 
 #' @details 
-#' Computed variables: \code{count}, the number of observations; \code{prop}, proportion on \code{x}; \code{y}, \code{y} position of labels.
+#' Computed variables: \code{count}, the number of observations; \code{prop}, proportion on \code{x}; \code{ylabel}, \code{y} position of labels.
 #' 
 #' @examples 
 #' ggplot(as.data.frame(Titanic)) + aes(x = Class, fill = Survived, weight = Freq) + geom_bar(position = "fill") + geom_text(stat = "fill_labels")
+#' ggplot(as.data.frame(Titanic)) + aes(x = Class, fill = Survived, weight = Freq) + geom_bar(position = "fill") + stat_fill_labels()
 #' ggplot(as.data.frame(Titanic)) + aes(x = Class, fill = Survived, weight = Freq) + geom_bar(position = "fill") + geom_text(stat = "fill_labels") + facet_grid(~Sex)
 #' ggplot(as.data.frame(Titanic)) + aes(x = as.integer(Class), fill = Survived, weight = Freq) + geom_area(position = "fill", stat = "count") + geom_text(stat = "fill_labels")
 #' @export
-stat_fill_labels <- function(mapping = NULL, data = NULL, geom = "bar",
-                       position = "stack", width = NULL, na.rm = FALSE, show.legend = NA, 
+stat_fill_labels <- function(mapping = NULL, data = NULL, geom = "text",
+                       position = "identity", width = NULL, na.rm = FALSE, show.legend = NA, 
                        inherit.aes = TRUE, ...) {
   layer(
     stat = StatFillLabels, data = data, mapping = mapping, geom = geom, 
