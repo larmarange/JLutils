3ab6629b42846f45d6e690d66f8e1a77ca195c4f
larmarange <joseph@larmarange.net>
2016-06-11 11:53:19 +0200

    bug fix in multiplot doc

--- R/multiplot.R
+++ R/multiplot.R
@@ -1,32 +1,32 @@
-#'  Multiple plot
-#'  
-#'  Renders multiple ggplot plots in one image
-#'  
-#'  @param ... ggplot objects
-#'  @param plotlist a list of ggplot objects
-#'  @param cols number of columns in layout
-#'  @param layout a matrix specifying the layout. if present, \code{cols} is ignored
-#'  
-#'  @note If the layout is something like \code{matrix(c(1,2,3,3), nrow=2, byrow=TRUE)}, 
-#'  then plot 1 will go in the upper left, 2 will go in the upper right, and 3 will go all 
-#'  the way across the bottom.
-#'  
-#'  @references Winston Chang, \emph{Cookbook for R}, 
-#'  \url{http://www.cookbook-r.com/Graphs/Multiple_graphs_on_one_page_(ggplot2)/}
-#'  
-#'  @examples 
-#'  require(ggplot2)
-#'  p1 <- ggplot(mtcars, aes(wt, mpg)) + geom_point()
-#'  p2 <- ggplot(mtcars, aes(factor(cyl))) + geom_bar()
-#'  p3 <- ggplot(mtcars, aes(factor(cyl), mpg)) + geom_violin()
-#'  p4 <- ggplot(mtcars, aes(factor(cyl), mpg)) + geom_boxplot()
-#'  
-#'  multiplot(p1, p2, p3, p4)
-#'  multiplot(p1, p2, p3, p4, cols = 2)
-#'  multiplot(p1, p2, p3, layout = matrix(c(1,2,3,3), nrow=2))
-#'  multiplot(p1, p2, p3, layout = matrix(c(1,2,3,3), nrow=2, byrow=TRUE))
-#'  
-#'  @export
+#' Multiple plot
+#' 
+#' Renders multiple ggplot plots in one image
+#' 
+#' @param ... ggplot objects
+#' @param plotlist a list of ggplot objects
+#' @param cols number of columns in layout
+#' @param layout a matrix specifying the layout. if present, \code{cols} is ignored
+#' 
+#' @note If the layout is something like \code{matrix(c(1,2,3,3), nrow=2, byrow=TRUE)}, 
+#' then plot 1 will go in the upper left, 2 will go in the upper right, and 3 will go all 
+#' the way across the bottom.
+#' 
+#' @references Winston Chang, \emph{Cookbook for R}, 
+#' \url{http://www.cookbook-r.com/Graphs/Multiple_graphs_on_one_page_(ggplot2)/}
+#' 
+#' @examples 
+#' require(ggplot2)
+#' p1 <- ggplot(mtcars, aes(wt, mpg)) + geom_point()
+#' p2 <- ggplot(mtcars, aes(factor(cyl))) + geom_bar()
+#' p3 <- ggplot(mtcars, aes(factor(cyl), mpg)) + geom_violin()
+#' p4 <- ggplot(mtcars, aes(factor(cyl), mpg)) + geom_boxplot()
+#' 
+#' multiplot(p1, p2, p3, p4)
+#' multiplot(p1, p2, p3, p4, cols = 2)
+#' multiplot(p1, p2, p3, layout = matrix(c(1,2,3,3), nrow=2))
+#' multiplot(p1, p2, p3, layout = matrix(c(1,2,3,3), nrow=2, byrow=TRUE))
+#' 
+#' @export
 
 # Source: http://www.cookbook-r.com/Graphs/Multiple_graphs_on_one_page_(ggplot2)/
 multiplot <- function(..., plotlist = NULL, cols =1, layout=NULL) {
