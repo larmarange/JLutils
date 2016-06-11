1d3de703911a01fac6b164093eb0bbcb0c57f9e7
larmarange <joseph@larmarange.net>
2016-06-11 11:32:28 +0200

    taking into account the fact that chisq.test doesn't always produce tables
    (cf. examples of chisq.test)

--- R/tidy_chisq.R
+++ R/tidy_chisq.R
@@ -12,18 +12,18 @@ tidy_chisq <- function(x, ...) {
   if (!all(c("observed", "expected", "residuals", "stdres") %in% names(x)))
     stop("x should have observed, expected, residuals and stdres entries.")
   
-  d <- length(dim(x$observed))
-  ret <- as.data.frame.table(x$observed)
+  d <- length(dimnames(as.table(x$observed)))
+  ret <- as.data.frame(as.table(x$observed))
   names(ret)[d+1] <- "observed"
   
-  ret <- cbind(ret, prop = as.data.frame.table(prop.table(x$observed))[[d+1]])
+  ret <- cbind(ret, prop = as.data.frame(prop.table(as.table(x$observed)))[[d+1]])
   if (d == 2) {
-    ret <- cbind(ret, row.prop = as.data.frame.table(prop.table(x$observed, 1))[[d+1]])
-    ret <- cbind(ret, col.prop = as.data.frame.table(prop.table(x$observed, 2))[[d+1]])
+    ret <- cbind(ret, row.prop = as.data.frame(prop.table(as.table(x$observed, 1)))[[d+1]])
+    ret <- cbind(ret, col.prop = as.data.frame(prop.table(as.table(x$observed, 2)))[[d+1]])
   }
   
-  ret <- cbind(ret, expected = as.data.frame.table(x$expected)[[d+1]])
-  ret <- cbind(ret, residuals = as.data.frame.table(x$residuals)[[d+1]])
-  ret <- cbind(ret, stdres = as.data.frame.table(x$stdres)[[d+1]])
+  ret <- cbind(ret, expected = as.data.frame(as.table(x$expected))[[d+1]])
+  ret <- cbind(ret, residuals = as.data.frame(as.table(x$residuals))[[d+1]])
+  ret <- cbind(ret, stdres = as.data.frame(as.table(x$stdres))[[d+1]])
   ret
 }
