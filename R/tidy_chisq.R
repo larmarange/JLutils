0dbbaed0d026a090ce4b74e7bd2e4725271c6778
larmarange <joseph@larmarange.net>
2016-06-11 11:05:16 +0200

    ret instead of res

--- R/tidy_chisq.R
+++ R/tidy_chisq.R
@@ -13,17 +13,17 @@ tidy_chisq <- function(x, ...) {
     stop("x should have observed, expected, residuals and stdres entries.")
   
   d <- length(dim(x$observed))
-  res <- as.data.frame.table(x$observed)
-  names(res)[d+1] <- "observed"
+  ret <- as.data.frame.table(x$observed)
+  names(ret)[d+1] <- "observed"
   
-  res <- cbind(res, prop = as.data.frame.table(prop.table(x$observed))[[d+1]])
+  ret <- cbind(ret, prop = as.data.frame.table(prop.table(x$observed))[[d+1]])
   if (d == 2) {
-    res <- cbind(res, row.prop = as.data.frame.table(prop.table(x$observed, 1))[[d+1]])
-    res <- cbind(res, col.prop = as.data.frame.table(prop.table(x$observed, 2))[[d+1]])
+    ret <- cbind(ret, row.prop = as.data.frame.table(prop.table(x$observed, 1))[[d+1]])
+    ret <- cbind(ret, col.prop = as.data.frame.table(prop.table(x$observed, 2))[[d+1]])
   }
   
-  res <- cbind(res, expected = as.data.frame.table(x$expected)[[d+1]])
-  res <- cbind(res, residuals = as.data.frame.table(x$residuals)[[d+1]])
-  res <- cbind(res, stdres = as.data.frame.table(x$stdres)[[d+1]])
-  res
+  ret <- cbind(ret, expected = as.data.frame.table(x$expected)[[d+1]])
+  ret <- cbind(ret, residuals = as.data.frame.table(x$residuals)[[d+1]])
+  ret <- cbind(ret, stdres = as.data.frame.table(x$stdres)[[d+1]])
+  ret
 }
