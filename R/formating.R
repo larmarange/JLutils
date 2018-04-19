0cc64970d53f1bbd6451cef6f74ab8dfcb9f8b10
Joseph Larmarange <joseph@larmarange.net>
2018-04-19 18:04:38 +0000

    reorganisation of formatters

--- R/formating.R
+++ R/formating.R
@@ -1,452 +1,264 @@
-#' Percent formatter (English)
+precision <- function(x) {
+  rng <- range(x, na.rm = TRUE)
+  
+  span <- if (zero_range(rng)) abs(rng[1]) else diff(rng)
+  if (span == 0)
+    return(1)
+  
+  10 ^ floor(log10(span))
+}
+
+
+#' Number formatters
 #' 
-#' Multiply by 100 and display a percent sign.
-#' 
+#' \code{number} is a generic formatter for numeric values.
+#' \code{en} is a shortcut for English format (comma as separator for thousands, point for decimal), \code{fr} for French format (space for thousands, comma for decimal).
+#' \code{percent} a shortcut for English percentages (value are multiplied by 100 and a % symbol is added) and \code{pourcent} a shortcut for French percentages.
+#' \code{comp_percent} returns the complement of 1, i.e. \code{percent(1 - x)}. \code{comp_pourcent} is the French version.
+#' \code{*_format} functions will return another functions, useful to be used for example with \code{ggplot2}.
+#' \code{en1} to \code{en5} are shortcuts for \code{en} with 1 to 5 digits after decimal point. Similarly, \code{fr1} to \code{fr5} are shortcuts of \code{fr},
+#' \code{percent1} to \code{percent5} of \code{percent}, etc.
+#'
+#' @return a formatted character vector or, for \code{*_format} functions, a function with single parameter \code{x}, a numeric vector, that
+#'   returns a character vector
 #' @param x a numeric vector to format
-#' @param digits number of decimal digits to display
-#' @details \code{percent0(x)} is a shortcut for \code{percent(x, digits = 0)}, 
-#' \code{percent1(x)} for \code{percent(x, digits = 1)}, etc.
-#' @export percent
+#' @param accuracy number to round to, \code{NULL} for automatic guess
+#' @param multiplier number to multiply by (e.g. for computing percentages or thousands)
+#' @param prefix,suffix Symbols to display before and after value
+#' @param big.mark character used between every 3 digits to separate thousands
+#' @param decimal.mark the character to be used to indicate the numeric decimal point
+#' @param ... other arguments passed on to \code{\link{format}}.
+#' @rdname number
+#' @export
 #' @examples
-#' percent(0.004)
-#' percent0(c(0.000045, 0.56789, 234.567))
-#' percent2(c(0.000045, 0.56789, 234.567))
-#' percent5(c(0.000045, 0.56789, 234.567))
-
-percent <- function (x, digits=1) 
-{
-  if (all(is.na(x)))
-    return("NA")
-  else
-    return(paste0(format(round(100*x, digits = digits), nsmall = digits, big.mark = "\u00A0", scientific = FALSE),"%"))
-}
-
-#' @rdname percent
-#' @export
-percent0 <- function (x)
-{
-  return(percent(x, digits = 0))
-}
-
-#' @rdname percent
-#' @export
-percent1 <- function (x)
-{
-  return(percent(x, digits = 1))
-}
-
-#' @rdname percent
-#' @export
-percent2 <- function (x)
-{
-  return(percent(x, digits = 2))
-}
-
-#' @rdname percent
-#' @export
-percent3 <- function (x)
-{
-  return(percent(x, digits = 3))
-}
-
-#' @rdname percent
-#' @export
-percent4 <- function (x)
-{
-  return(percent(x, digits = 4))
-}
-
-#' @rdname percent
-#' @export
-percent5 <- function (x)
-{
-  return(percent(x, digits = 5))
-}
-
-#' @rdname percent
-#' @details \code{comp.percent(x)} returns the complement to 1 of \code{x}, 
-#' i.e. \code{percent(1 - x)}.
-#' @examples 
-#' comp.percent(0.255)
-#' @export
-comp.percent <- function (x, digits=1) 
-{
-  return(percent(1 - x, digits))
-}
-
-#' @rdname percent
-#' @export
-comp.percent0 <- function (x)
-{
-  return(comp.percent(x, digits = 0))
-}
-
-#' @rdname percent
-#' @export
-comp.percent1 <- function (x)
-{
-  return(comp.percent(x, digits = 1))
-}
-
-#' @rdname percent
-#' @export
-comp.percent2 <- function (x)
-{
-  return(comp.percent(x, digits = 2))
-}
-
-#' @rdname percent
-#' @export
-comp.percent3 <- function (x)
-{
-  return(comp.percent(x, digits = 3))
-}
-
-#' @rdname percent
-#' @export
-comp.percent4 <- function (x)
-{
-  return(comp.percent(x, digits = 4))
-}
-
-#' @rdname percent
-#' @export
-comp.percent5 <- function (x)
-{
-  return(comp.percent(x, digits = 5))
-}
-
-
-#' Percent formatter (French)
+#' v <- c(12.3, 4, 12345.789, 0.0002)
+#' number(v)
+#' en(v)
+#' fr(v)
+#' en2(v)
+#' en(v, accuracy = .001)
+#' en(v, accuracy = .5)
 #' 
-#' Multiply by 100, use a comma as decimal separator and display a percent sign (French way).
-#' 
-#' @param x a numeric vector to format
-#' @param digits number of decimal digits to display
-#' @details \code{pourcent0(x)} is a shortcut for \code{pourcent(x, digits = 0)}, 
-#' \code{pourcent1(x)} for \code{pourcent(x, digits = 1)}, etc.
-#' @export pourcent
-#' @examples
-#' pourcent(0.004)
-#' pourcent0(c(0.000045, 0.56789, 234.567))
-#' pourcent2(c(0.000045, 0.56789, 234.567))
-#' pourcent5(c(0.000045, 0.56789, 234.567))
-
-pourcent <- function (x, digits=1) 
-{
-  if (all(is.na(x)))
-    return("NA")
-  else
-    return(paste0(format(
-      round(100*x, digits = digits), 
-      nsmall = digits, big.mark = " ", 
-      scientific = FALSE, decimal.mark = ","
-      ), " %"))
+#' p <- runif(10)
+#' p
+#' percent(p)
+#' percent2(p)
+#' pourcent2(p)
+#' comp_percent(p)
+#'
+#' # Per mille
+#' per_mille <- number_format(multiplier = 1000, suffix = "\u2030", accuracy = .1)
+#' per_mille(v)
+number_format <- function(accuracy = 1, multiplier = 1, prefix = "", suffix = "", big.mark = " ", decimal.mark = ".", ...) {
+  function(x) number(x, accuracy, multiplier, prefix, suffix, big.mark, decimal.mark, ...)
 }
-
-#' @rdname pourcent
 #' @export
-pourcent0 <- function (x)
-{
-  return(pourcent(x, digits = 0))
+#' @rdname number
+number <- function(x, accuracy = 1, multiplier = 1, prefix = "", suffix = "", big.mark = " ", decimal.mark = ".", ...) {
+  if (length(x) == 0) return(character())
+  if (is.null(accuracy)) {
+    x <- round_any(x, precision(x) / multiplier)
+    nsmall <- -floor(log10(precision(x)))
+  } else {
+    x <- round_any(x, accuracy / multiplier)
+    nsmall <- -floor(log10(accuracy))
+  }
+  nsmall <- min(max(nsmall, 0), 20)
+  paste0(prefix, format(multiplier * x, big.mark = big.mark, decimal.mark = decimal.mark, scientific = FALSE, trim = TRUE, nsmall = nsmall, ...), suffix)
 }
 
-#' @rdname pourcent
 #' @export
-pourcent1 <- function (x)
-{
-  return(pourcent(x, digits = 1))
+#' @rdname number
+en_format <- function(accuracy = 1, multiplier = 1, prefix = "", suffix = "", big.mark = ",", decimal.mark = ".", ...) {
+  number_format(accuracy, multiplier, prefix, suffix, big.mark, decimal.mark, ...)
 }
-
-#' @rdname pourcent
 #' @export
-pourcent2 <- function (x)
-{
-  return(pourcent(x, digits = 2))
+#' @rdname number
+en <- function(x, accuracy = 1, multiplier = 1, prefix = "", suffix = "", big.mark = ",", decimal.mark = ".", ...) {
+  number(x, accuracy, multiplier, prefix, suffix, big.mark, decimal.mark, ...)
 }
 
-#' @rdname pourcent
 #' @export
-pourcent3 <- function (x)
-{
-  return(pourcent(x, digits = 3))
+#' @rdname number
+fr_format <- function(accuracy = 1, multiplier = 1, prefix = "", suffix = "", big.mark = " ", decimal.mark = ",", ...) {
+  number_format(accuracy, multiplier, prefix, suffix, big.mark, decimal.mark, ...)
 }
-
-#' @rdname pourcent
 #' @export
-pourcent4 <- function (x)
-{
-  return(pourcent(x, digits = 4))
+#' @rdname number
+fr <- function(x, accuracy = 1, multiplier = 1, prefix = "", suffix = "", big.mark = " ", decimal.mark = ",", ...) {
+  number(x, accuracy, multiplier, prefix, suffix, big.mark, decimal.mark, ...)
 }
 
-#' @rdname pourcent
 #' @export
-pourcent5 <- function (x)
-{
-  return(pourcent(x, digits = 5))
+#' @rdname number
+percent_format <- function(accuracy = 1, multiplier = 100, prefix = "", suffix = "%", big.mark = " ", decimal.mark = ".", ...) {
+  number_format(accuracy, multiplier, prefix, suffix, big.mark, decimal.mark, ...)
 }
-
-#' @rdname pourcent
-#' @details \code{comp.pourcent(x)} returns the complement to 1 of \code{x}, 
-#' i.e. \code{pourcent(1 - x)}.
-#' @examples 
-#' comp.pourcent(0.255)
 #' @export
-comp.pourcent <- function (x, digits=1) 
-{
-  return(pourcent(1 - x, digits))
+#' @rdname number
+percent <- function(x, accuracy = 1, multiplier = 100, prefix = "", suffix = "%", big.mark = " ", decimal.mark = ".", ...) {
+  number(x, accuracy, multiplier, prefix, suffix, big.mark, decimal.mark, ...)
 }
 
-#' @rdname pourcent
 #' @export
-comp.pourcent0 <- function (x)
-{
-  return(comp.pourcent(x, digits = 0))
+#' @rdname number
+pourcent_format <- function(accuracy = 1, multiplier = 100, prefix = "", suffix = " %", big.mark = " ", decimal.mark = ",", ...) {
+  number_format(accuracy, multiplier, prefix, suffix, big.mark, decimal.mark, ...)
 }
-
-#' @rdname pourcent
 #' @export
-comp.pourcent1 <- function (x)
-{
-  return(comp.pourcent(x, digits = 1))
+#' @rdname number
+pourcent <- function(x, accuracy = 1, multiplier = 100, prefix = "", suffix = " %", big.mark = " ", decimal.mark = ",", ...) {
+  number(x, accuracy, multiplier, prefix, suffix, big.mark, decimal.mark, ...)
 }
 
-#' @rdname pourcent
 #' @export
-comp.pourcent2 <- function (x)
-{
-  return(comp.pourcent(x, digits = 2))
+#' @rdname number
+comp_percent_format <- function(accuracy = 1, multiplier = 100, prefix = "", suffix = "%", big.mark = " ", decimal.mark = ".", ...) {
+  function(x) {
+    comp_percent(x, accuracy, multiplier, prefix, suffix, big.mark, decimal.mark, ...)
+  }
 }
 
-#' @rdname pourcent
 #' @export
-comp.pourcent3 <- function (x)
-{
-  return(comp.pourcent(x, digits = 3))
+#' @rdname number
+comp_percent <- function(x, accuracy = 1, multiplier = 100, prefix = "", suffix = "%", big.mark = " ", decimal.mark = ".", ...) {
+  number(1 - x, accuracy, multiplier, prefix, suffix, big.mark, decimal.mark, ...)
 }
 
-#' @rdname pourcent
 #' @export
-comp.pourcent4 <- function (x)
-{
-  return(comp.pourcent(x, digits = 4))
+#' @rdname number
+comp_pourcent_format <- function(accuracy = 1, multiplier = 100, prefix = "", suffix = " %", big.mark = " ", decimal.mark = ",", ...) {
+  function(x) {
+    comp_pourcent(x, accuracy, multiplier, prefix, suffix, big.mark, decimal.mark, ...)
+  }
 }
 
-#' @rdname pourcent
 #' @export
-comp.pourcent5 <- function (x)
-{
-  return(comp.pourcent(x, digits = 5))
+#' @rdname number
+comp_pourcent <- function(x, accuracy = 1, multiplier = 100, prefix = "", suffix = " %", big.mark = " ", decimal.mark = ",", ...) {
+  number(1 - x, accuracy, multiplier, prefix, suffix, big.mark, decimal.mark, ...)
 }
 
-#' English formatter
-#' 
-#' Use a dot as decimal separator and a single quote as separator for thousands.
-#' 
-#' @param x a numeric vector to format
-#' @param digits number of decimal digits to display
-#' @details \code{en0(x)} is a shortcut for \code{en(x, digits = 0)}, 
-#' \code{en1(x)} for \code{en(x, digits = 1)}, etc.
-#' @export en
-#' @examples
-#' en(12345.67)
-#' en0(c(0.123, 123.4567, 1234567.89))
-#' en2(c(0.123, 123.4567, 1234567.89))
-#' en5(c(0.123, 123.4567, 1234567.89))
 
-en <- function (x, digits=1) 
-{
-  if (all(is.na(x)))
-    return("NA")
-  else
-    return(format(
-      round(x, digits = digits), 
-      nsmall = digits, big.mark = "'", 
-      scientific = FALSE, decimal.mark = "."
-    ))
-}
-
-#' @rdname en
+#' @rdname number
 #' @export
-en0 <- function (x)
-{
-  return(en(x, digits = 0))
-}
+en1 <- en_format(accuracy = .1)
 
-#' @rdname en
+#' @rdname number
 #' @export
-en1 <- function (x)
-{
-  return(en(x, digits = 1))
-}
+en2 <- en_format(accuracy = .01)
 
-#' @rdname en
+#' @rdname number
 #' @export
-en2 <- function (x)
-{
-  return(en(x, digits = 2))
-}
+en3 <- en_format(accuracy = .001)
 
-#' @rdname en
+#' @rdname number
 #' @export
-en3 <- function (x)
-{
-  return(en(x, digits = 3))
-}
+en4 <- en_format(accuracy = .0001)
 
-#' @rdname en
+#' @rdname number
 #' @export
-en4 <- function (x)
-{
-  return(en(x, digits = 4))
-}
+en5 <- en_format(accuracy = .00001)
 
-#' @rdname en
+#' @rdname number
 #' @export
-en5 <- function (x)
-{
-  return(en(x, digits = 5))
-}
+fr1 <- fr_format(accuracy = .1)
 
-
-#' French formatter
-#' 
-#' Use a comma as decimal separator and a space as separator for thousands.
-#' 
-#' @param x a numeric vector to format
-#' @param digits number of decimal digits to display
-#' @details \code{fr0(x)} is a shortcut for \code{fr(x, digits = 0)}, 
-#' \code{fr1(x)} for \code{fr(x, digits = 1)}, etc.
-#' @export fr
-#' @examples
-#' fr(12345.57)
-#' fr0(c(0.123, 123.4567, 1234567.89))
-#' fr2(c(0.123, 123.4567, 1234567.89))
-#' fr5(c(0.123, 123.4567, 1234567.89))
-
-fr <- function (x, digits=1) 
-{
-  if (all(is.na(x)))
-    return("NA")
-  else
-    return(format(
-      round(x, digits = digits), 
-      nsmall = digits, big.mark = " ", 
-      scientific = FALSE, decimal.mark = ","
-    ))
-}
-
-#' @rdname fr
+#' @rdname number
 #' @export
-fr0 <- function (x)
-{
-  return(fr(x, digits = 0))
-}
+fr2 <- fr_format(accuracy = .01)
 
-#' @rdname fr
+#' @rdname number
 #' @export
-fr1 <- function (x)
-{
-  return(fr(x, digits = 1))
-}
+fr3 <- fr_format(accuracy = .001)
 
-#' @rdname fr
+#' @rdname number
 #' @export
-fr2 <- function (x)
-{
-  return(fr(x, digits = 2))
-}
+fr4 <- fr_format(accuracy = .0001)
 
-#' @rdname fr
+#' @rdname number
 #' @export
-fr3 <- function (x)
-{
-  return(fr(x, digits = 3))
-}
+fr5 <- fr_format(accuracy = .00001)
 
-#' @rdname fr
+#' @rdname number
 #' @export
-fr4 <- function (x)
-{
-  return(fr(x, digits = 4))
-}
+percent1 <- percent_format(accuracy = .1)
 
-#' @rdname fr
+#' @rdname number
 #' @export
-fr5 <- function (x)
-{
-  return(fr(x, digits = 5))
-}
+percent2 <- percent_format(accuracy = .01)
 
-#' International formatter
-#' 
-#' Use a dot as decimal separator and a space as separator for thousands.
-#' 
-#' @param x a numeric vector to format
-#' @param digits number of decimal digits to display
-#' @details \code{int0(x)} is a shortcut for \code{int(x, digits = 0)}, 
-#' \code{int1(x)} for \code{int(x, digits = 1)}, etc.
-#' @export int
-#' @examples
-#' int(12345.67)
-#' int0(c(0.123, 123.4567, 1234567.89))
-#' int2(c(0.123, 123.4567, 1234567.89))
-#' int5(c(0.123, 123.4567, 1234567.89))
-
-int <- function (x, digits=1) 
-{
-  if (all(is.na(x)))
-    return("NA")
-  else
-    return(format(
-      round(x, digits = digits), 
-      nsmall = digits, big.mark = " ", 
-      sciinttific = FALSE, decimal.mark = "."
-    ))
-}
-
-#' @rdname int
+#' @rdname number
 #' @export
-int0 <- function (x)
-{
-  return(int(x, digits = 0))
-}
+percent3 <- percent_format(accuracy = .001)
 
-#' @rdname int
+#' @rdname number
 #' @export
-int1 <- function (x)
-{
-  return(int(x, digits = 1))
-}
+percent4 <- percent_format(accuracy = .0001)
 
-#' @rdname int
+#' @rdname number
 #' @export
-int2 <- function (x)
-{
-  return(int(x, digits = 2))
-}
+percent5 <- percent_format(accuracy = .00001)
 
-#' @rdname int
+#' @rdname number
 #' @export
-int3 <- function (x)
-{
-  return(int(x, digits = 3))
-}
+pourcent1 <- pourcent_format(accuracy = .1)
 
-#' @rdname int
+#' @rdname number
 #' @export
-int4 <- function (x)
-{
-  return(int(x, digits = 4))
-}
+pourcent2 <- pourcent_format(accuracy = .01)
 
-#' @rdname int
+#' @rdname number
 #' @export
-int5 <- function (x)
-{
-  return(int(x, digits = 5))
-}
+pourcent3 <- pourcent_format(accuracy = .001)
 
+#' @rdname number
+#' @export
+pourcent4 <- pourcent_format(accuracy = .0001)
+
+#' @rdname number
+#' @export
+pourcent5 <- pourcent_format(accuracy = .00001)
+
+#' @rdname number
+#' @export
+comp_percent1 <- comp_percent_format(accuracy = .1)
+
+#' @rdname number
+#' @export
+comp_percent2 <- comp_percent_format(accuracy = .01)
+
+#' @rdname number
+#' @export
+comp_percent3 <- comp_percent_format(accuracy = .001)
+
+#' @rdname number
+#' @export
+comp_percent4 <- comp_percent_format(accuracy = .0001)
+
+#' @rdname number
+#' @export
+comp_percent5 <- comp_percent_format(accuracy = .00001)
+
+#' @rdname number
+#' @export
+comp_pourcent1 <- comp_pourcent_format(accuracy = .1)
+
+#' @rdname number
+#' @export
+comp_pourcent2 <- comp_pourcent_format(accuracy = .01)
+
+#' @rdname number
+#' @export
+comp_pourcent3 <- comp_pourcent_format(accuracy = .001)
+
+#' @rdname number
+#' @export
+comp_pourcent4 <- comp_pourcent_format(accuracy = .0001)
+
+#' @rdname number
+#' @export
+comp_pourcent5 <- comp_pourcent_format(accuracy = .00001)
 
 
 #' Formatting numbers like in WHO publications
@@ -464,15 +276,15 @@ int5 <- function (x)
 #' 
 #' @export
 #' @examples 
-#' who.format(c(3, 8, 42, 75, 45678, 9876543, 12345678))
-who.format <- function(x) {
+#' who_formatter(c(3, 8, 42, 75, 45678, 9876543, 12345678))
+who_formatter <- function(x) {
   x[x < 4 & !is.na(x)] <- 0
   x[x >= 5 & x < 10 & !is.na(x)] <- 10
   x[x >= 10 & x < 100 & !is.na(x)] <- signif(x[x >= 10 & x < 100 & !is.na(x)], digits=1)
   x[x >= 100 & x < 10000000 & !is.na(x)] <- signif(x[x >= 100 & x < 10000000 & !is.na(x)], digits=2)
   x[x >= 10000000 & !is.na(x)] <- signif(x[x >= 10000000 & !is.na(x)], digits=3)
   
-  return(format(x, digits=0, big.mark="\u00A0", scientific=FALSE))
+  return(format(x, digits=0, big.mark=" ", scientific=FALSE))
 }
 
 
@@ -481,24 +293,25 @@ who.format <- function(x) {
 #' Formatter for p-values, adding a symbol "<" for very small p-values and, optionally, significance stars
 #' 
 #' @param x a numeric vector of p-values
-#' @param digits number of decimal digits to display
-#' @param formatter a formatter function, typically \code{\link{en}}, \code{\link{fr}} or \code{\link{int}}
+#' @param formatter a formatter function, see \code{\link{number}}, typically \code{\link{en}} or \code{\link{fr}}
 #' @param stars add significance stars?
 #' @param three level below which to display three stars '***'
 #' @param two level below which to display two stars '**'
 #' @param one level below which to display one star '*'
 #' @param point level below which to display a point '.' (\code{NULL} to not display a point)
+#' @param ... additional parameters sent to formatter function
 #' @details 
 #' \code{pval_format} will produce a custom function, to be used for example with \code{ggplot2}.
 #' @export
 #' @examples 
 #' p <- c(.50, 0.12, .09, .045, .011, .009, .00002, NA)
 #' pval(p)
-#' pval(p, digits = 2, formatter = fr)
+#' pval(p, formatter = fr, accuracy = .01)
 #' pval(p, stars = TRUE)
-pval <- function(x, digits = 3, formatter = en, stars = FALSE, three = 0.001, two = 0.01, one = 0.05, point = 0.1) {
-  res <- formatter(x, digits = digits)
-  res[x < 10 ^ -digits] <- paste0("<", formatter(10 ^ -digits, digits = digits))
+pval <- function(x, formatter = en, accuracy = .001, stars = FALSE, three = 0.001, two = 0.01, one = 0.05, point = 0.1, ...) {
+  res <- formatter(x, accuracy = accuracy, ...)
+  digits <- -floor(log10(accuracy))
+  res[x < 10 ^ -digits] <- paste0("<", formatter(10 ^ -digits, accuracy = accuracy, ...))
   if (stars)
     res <- paste(res, signif_stars(x, three, two, one, point))
   res
@@ -509,13 +322,11 @@ pval <- function(x, digits = 3, formatter = en, stars = FALSE, three = 0.001, tw
 #' @examples 
 #' custom_function <- pval_format(digits = 1, stars = TRUE)
 #' custom_function(p)
-pval_format <- function(digits = 3, formatter = en, stars = FALSE, three = 0.001, two = 0.01, one = 0.05, point = 0.1) {
+pval_format <- function(formatter = en, accuracy = .001, stars = FALSE, three = 0.001, two = 0.01, one = 0.05, point = 0.1) {
   function(x) {
-    pval(x, digits, formatter, stars, three, two, one, point)
+    pval(x, formatter, accuracy, stars, three, two, one, point)
   }
 }
-
-
 
 #' @rdname pval
 #' @export signif_stars
@@ -532,6 +343,8 @@ signif_stars <- function(x, three = 0.001, two = 0.01, one = 0.05, point = 0.1) 
     res[x <= two] <- "**"
   if (!is.null(three))
     res[x <= three] <- "***"
-  res[is.na(x)] <- NA
   res
 }
+
+
+
