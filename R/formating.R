1b01ffd4c94e837a9cf5ccd6f25c7eaae8489f66
Joseph Larmarange <joseph@larmarange.net>
2018-06-06 12:23:56 +0200

    leading_zeros

--- R/formating.R
+++ R/formating.R
@@ -375,5 +375,31 @@ signif_stars <- function(x, three = 0.001, two = 0.01, one = 0.05, point = 0.1) 
   res
 }
 
-
-
+#' Add leading zeros
+#' 
+#' @param x a numeric vector
+#' @param left_digits number of digits before decimal point, automatically computed if not provided
+#' @param digits number of digits after decimal point
+#' @param prefix,suffix Symbols to display before and after value
+#' @param ... additional paramaters passed to \code{\link[base]{formatC}}, as \code{big.mark} or \code{decimal.mark}
+#' @export
+#' @examples 
+#' v <- c(2, 103.24, 1042.147, 12.4566, NA)
+#' leading_zeros(v)
+#' leading_zeros(v, digits = 1)
+#' leading_zeros(v, left_digits = 6, big.mark = " ")
+#' leading_zeros(c(0, 6, 12, 18), prefix = "M")
+leading_zeros <- function(x, left_digits = NULL, digits = 0, prefix = "", suffix = "", ...) {
+  if(is.null(left_digits))
+    left_digits <- trunc(max(log10(x), na.rm = TRUE)) + 1
+  if (digits > 0) {
+    width <- left_digits + digits + 1
+  } else {
+    width <- left_digits
+  }
+  paste0(
+    prefix,
+    formatC(x, width = width, digits = digits, flag = "0", format = "f", preserve.width = "common", ...),
+    suffix
+  )
+}
