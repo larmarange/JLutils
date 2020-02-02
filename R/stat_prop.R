88bd358a9d555459d5aa391146201d81e2e308fd
Joseph Larmarange <joseph@larmarange.net>
2020-02-02 23:58:24 +0100

    compatibility with current version of ggplot2

--- R/stat_prop.R
+++ R/stat_prop.R
@@ -55,10 +55,7 @@ stat_prop <- function(mapping = NULL, data = NULL,
     ...
   )
   if (!is.null(params$y)) {
-    abort("stat_prop() must not be used with a y aesthetic.")
-  }
-  if (!is.null(params$prop_of)) {
-    abort("stat_prop() must not be used with a prop_of aesthetic.")
+    stop("stat_prop() must not be used with a y aesthetic.", call. = FALSE)
   }
   
   layer(
@@ -78,49 +75,36 @@ stat_prop <- function(mapping = NULL, data = NULL,
 #' @usage NULL
 #' @export
 StatProp <- ggproto("StatProp", Stat,
-                     required_aes = c("x|y", "prop_of"),
-                     
-                     default_aes = aes(
-                       x = after_stat(count), y = after_stat(count), 
-                       label = scales::percent(after_stat(prop)), 
-                       weight = 1
-                      ),
-                     
-                     setup_params = function(data, params) {
-                       params$flipped_aes <- has_flipped_aes(data, params, main_is_orthogonal = FALSE)
-                       
-                       has_x <- !(is.null(data$x) && is.null(params$x))
-                       has_y <- !(is.null(data$y) && is.null(params$y))
-                       if (!has_x && !has_y) {
-                         abort("stat_prop() requires an x or y aesthetic.")
-                       }
-                       if (has_x && has_y) {
-                         abort("stat_prop() can only have an x or y aesthetic.")
-                       }
-                       
-                       params
-                     },
-                     
-                     extra_params = c("na.rm", "orientation"),
-                     
-                     compute_panel = function(self, data, scales, width = NULL, flipped_aes = FALSE) {
-                       data <- ggplot2::flip_data(data, flipped_aes)
-                       data$weight <- data$weight %||% rep(1, nrow(data))
-                       width <- width %||% (ggplot2::resolution(data$x) * 0.9)
-                       
-                       # sum weights for each combination of prop_of and aesthetics
-                       # the use of . allows to consider all aesthetics defined in data
-                       panel <- aggregate(weight ~ ., data = data, sum, na.rm = TRUE)
-                       names(panel)[which(names(panel) == "weight")] <- "count"
-                       panel$count[is.na(panel$count)] <- 0
-                       
-                       # compute proportions by prop_of
-                       f <- function(x) {sum(abs(x))}
-                       panel$prop <- panel$count / ave(panel$count, panel$prop_of, FUN = f)
-                       panel$width <- width
-
-                       flip_data(panel, flipped_aes)
-                     }
+  required_aes = c("x", "prop_of"),
+  default_aes = aes(
+    y = stat(count), weight = 1,
+    label = scales::percent(..prop..)
+  ),
+  
+  setup_params = function(data, params) {
+    if (!is.null(data$y)) {
+      stop("stat_prop() must not be used with a y aesthetic.", call. = FALSE)
+    }
+    params
+  },       
+                    
+  compute_panel = function(self, data, scales, width = NULL) {
+   data$weight <- data$weight %||% rep(1, nrow(data))
+   width <- width %||% (ggplot2::resolution(data$x) * 0.9)
+   
+   # sum weights for each combination of prop_of and aesthetics
+   # the use of . allows to consider all aesthetics defined in data
+   panel <- aggregate(weight ~ ., data = data, sum, na.rm = TRUE)
+   names(panel)[which(names(panel) == "weight")] <- "count"
+   panel$count[is.na(panel$count)] <- 0
+   
+   # compute proportions by prop_of
+   f <- function(x) {sum(abs(x))}
+   panel$prop <- panel$count / ave(panel$count, panel$prop_of, FUN = f)
+   panel$width <- width
+  
+   panel
+  }
 )
 
 
