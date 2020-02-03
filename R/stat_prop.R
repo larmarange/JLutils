c05aa135f37f903fa34c86d967dffac858c2e13e
Joseph Larmarange <joseph@larmarange.net>
2020-02-03 08:39:43 +0100

    finally, prop_by seems more appropriate

--- R/stat_prop.R
+++ R/stat_prop.R
@@ -1,13 +1,13 @@
 #' Compute proportions according to custom denominator
 #' 
 #' `stat_prop()` is a variation of `stat_count()` allowing to compute custom 
-#' proportions according to the `prop_among` aesthetic defining the denominator
-#' (i.e. all proportions for a same value of `prop_among` will sum to 1)
+#' proportions according to the `prop_by` aesthetic defining the denominator
+#' (i.e. all proportions for a same value of `prop_by` will sum to 1)
 #' 
 #' @inheritParams ggplot2::stat_count
 #' @param geom Override the default connection between `geom_bar()` and `stat_prop()`.
 #' @section Aesthetics:
-#' `stat_prop()` requires the \strong{prop_among} aesthetic.
+#' `stat_prop()` requires the \strong{prop_by} aesthetic.
 #' @section Computed variables:
 #' \describe{
 #'   \item{count}{number of points in bin}
@@ -23,19 +23,19 @@
 #' d <- as.data.frame(Titanic)
 #' 
 #' p <- ggplot(d) +
-#'   aes(x = Class, fill = Survived, weight = Freq, prop_among = Class) +
+#'   aes(x = Class, fill = Survived, weight = Freq, prop_by = Class) +
 #'   geom_bar(position = "fill") + 
 #'   geom_text(stat = "prop", position = position_fill(.5))
 #' p
 #' p + facet_grid(~ Sex)
 #' 
 #' ggplot(d) +
-#'   aes(x = Class, fill = Survived, weight = Freq, prop_among = Survived) +
+#'   aes(x = Class, fill = Survived, weight = Freq, prop_by = Survived) +
 #'   geom_bar(position = "dodge") +
 #'   geom_text(stat = "prop", position = position_dodge(0.9), vjust = "bottom")
 #'   
 #' ggplot(d) +
-#'   aes(x = Class, fill = Survived, weight = Freq, prop_among = 1) +
+#'   aes(x = Class, fill = Survived, weight = Freq, prop_by = 1) +
 #'   geom_bar() +
 #'   geom_text(
 #'     aes(label = scales::percent(..prop.., accuracy = .1)), 
@@ -78,7 +78,7 @@ stat_prop <- function(mapping = NULL, data = NULL,
 #' @usage NULL
 #' @export
 StatProp <- ggproto("StatProp", Stat,
-  required_aes = c("x", "prop_among"),
+  required_aes = c("x", "prop_by"),
   default_aes = aes(
     y = stat(count), weight = 1,
     label = scales::percent(..prop..)
@@ -95,15 +95,15 @@ StatProp <- ggproto("StatProp", Stat,
    data$weight <- data$weight %||% rep(1, nrow(data))
    width <- width %||% (ggplot2::resolution(data$x) * 0.9)
    
-   # sum weights for each combination of prop_among and aesthetics
+   # sum weights for each combination of prop_by and aesthetics
    # the use of . allows to consider all aesthetics defined in data
    panel <- aggregate(weight ~ ., data = data, sum, na.rm = TRUE)
    names(panel)[which(names(panel) == "weight")] <- "count"
    panel$count[is.na(panel$count)] <- 0
    
-   # compute proportions by prop_among
+   # compute proportions by prop_by
    f <- function(x) {sum(abs(x))}
-   panel$prop <- panel$count / ave(panel$count, panel$prop_among, FUN = f)
+   panel$prop <- panel$count / ave(panel$count, panel$prop_by, FUN = f)
    panel$width <- width
   
    panel
