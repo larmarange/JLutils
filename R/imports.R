aa8320664719915aa341ca142ca3504c3de6f022
Joseph Larmarange <joseph@larmarange.net>
2020-12-02 19:48:06 +0100

    ggcoef_model is now in GGally

--- R/imports.R
+++ R/imports.R
@@ -13,3 +13,27 @@ GGally::stat_cross
 #' @importFrom questionr ggsurvey
 #' @export
 questionr::ggsurvey
+
+#' @importFrom GGally geom_stripped_rows
+#' @export
+GGally::geom_stripped_rows
+
+#' @importFrom GGally geom_stripped_cols
+#' @export
+GGally::geom_stripped_cols
+
+#' @importFrom GGally ggcoef_model
+#' @export
+GGally::ggcoef_model
+
+#' @importFrom GGally ggcoef_compare
+#' @export
+GGally::ggcoef_compare
+
+#' @importFrom GGally ggcoef_multinom
+#' @export
+GGally::ggcoef_multinom
+
+#' @importFrom GGally ggcoef_plot
+#' @export
+GGally::ggcoef_plot
