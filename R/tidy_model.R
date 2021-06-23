8bdfb98a03bf0b60f5c33fbf4457be68ac51400e
Joseph <joseph@larmarange.net>
2021-06-23 18:01:37 +0200

    tidy_model() update

--- R/tidy_model.R
+++ R/tidy_model.R
@@ -16,16 +16,16 @@ tidy_model <- function(x, conf.int = FALSE, conf.level = .95, exponentiate = FAL
   if (quick) {
     co <- coef(x)
     ret <- data.frame(term = names(co), estimate = unname(co), stringsAsFactors = FALSE)
-    return(process_model(ret, x, conf.int = FALSE, exponentiate = exponentiate))
+    return(.process_model(ret, x, conf.int = FALSE, exponentiate = exponentiate))
   }
   co <- coef(summary(x))
   nn <- c("estimate", "std.error", "statistic", "p.value")
-  ret <- broom:::fix_data_frame(co, nn[1:ncol(co)])
-  process_model(ret, x, conf.int = conf.int, conf.level = conf.level, exponentiate = exponentiate)
+  ret <- .fix_data_frame(co, nn[1:ncol(co)])
+  .process_model(ret, x, conf.int = conf.int, conf.level = conf.level, exponentiate = exponentiate)
 }
 
 
-process_model <- function(ret, x, conf.int = FALSE, conf.level = .95, exponentiate = FALSE) {
+.process_model <- function(ret, x, conf.int = FALSE, conf.level = .95, exponentiate = FALSE) {
   if (exponentiate) {
     trans <- exp
   } else {
@@ -38,9 +38,37 @@ process_model <- function(ret, x, conf.int = FALSE, conf.level = .95, exponentia
     colnames(CI) <- c("conf.low", "conf.high")
     CI <- as.data.frame(CI)
     CI$term <- rownames(CI)
-    ret <- merge(ret, unrowname(CI), by = "term", all.x = TRUE)
+    ret <- merge(ret, .unrowname(CI), by = "term", all.x = TRUE)
   }
 
   ret$estimate <- trans(ret$estimate)
   ret
+}
+
+
+.fix_data_frame<- function (x, newnames = NULL, newcol = "term") 
+{
+  if (!is.null(newnames) && length(newnames) != ncol(x)) {
+    stop("newnames must be NULL or have length equal to number of columns")
+  }
+  if (all(rownames(x) == seq_len(nrow(x)))) {
+    ret <- data.frame(x, stringsAsFactors = FALSE)
+    if (!is.null(newnames)) {
+      colnames(ret) <- newnames
+    }
+  }
+  else {
+    ret <- data.frame(...new.col... = rownames(x), .unrowname(x), 
+                      stringsAsFactors = FALSE)
+    colnames(ret)[1] <- newcol
+    if (!is.null(newnames)) {
+      colnames(ret)[-1] <- newnames
+    }
+  }
+  tibble::as_tibble(ret)
+}
+
+.unrowname <- function(x) {
+  rownames(x) <- NULL
+  x
 }
